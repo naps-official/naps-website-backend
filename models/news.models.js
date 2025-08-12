c3c185af14ccc426ec207d45a1849cf36c3d6b70
PaulAjii <paulajii134@gmail.com>
2025-08-12 11:08:52 +0100

    Add: News Schema

---
+++ models/news.models.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const newsSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: [10, "Title must be at least 10 characters long"]
+    },
+
+    content: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: [50, "Content must be at least 50 characters long"]
+    },
+
+    image: {
+        type: String,
+    },
+
+    publishedAt: {
+        type: Date,
+        default: Date.now,
+    },
+
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+
+    status: {
+        type: String,
+        enum: ["draft", "published", "archived"],
+        default: "draft",
+    },
+
+    viewCount: {
+        type: Number,
+        default: 0,
+    }
+},
+    { timestamps: true}
+);
+
+const News = mongoose.model("New", newsSchema);
+
+export default News;
