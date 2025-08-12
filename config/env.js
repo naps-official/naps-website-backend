f777cedd9caec4e898261751736fcdb7168a99ae
PaulAjii <paulajii134@gmail.com>
2025-08-12 13:49:11 +0100

    feat: added jwt secret and expiration

--- config/env.js
+++ config/env.js
@@ -1,4 +1,4 @@
 import { config } from "dotenv";
 config({path: `.env.${process.env.NODE_ENV || "development"}.local`}, {debug: true})
 
-export const { PORT, NODE_ENV, MONGO_URI } = process.env
+export const { PORT, NODE_ENV, MONGO_URI, JWT_SECRET, JWT_EXPIRATION } = process.env
