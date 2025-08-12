f77f0a87b992d614377fe9431bfb201f08f3892d
PaulAjii <paulajii134@gmail.com>
2025-08-12 16:02:31 +0100

    fix: add getUserById route to user routes

--- routes/users.route.js
+++ routes/users.route.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 
-import { createUser, getUsers } from "../controllers/user.controller.js";
+import { createUser, getUsers, getUserById } from "../controllers/user.controller.js";
 import validateUser from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.post("/", validateUser("President"), createUser);
 router.get("/", getUsers);
+router.post("/", validateUser("President"), createUser);
+router.get("/:id", getUserById);
 
 export default router;
