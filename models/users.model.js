af6d9063b66c0e2686ec2a6c2a3c4b7eb12a2d85
PaulAjii <paulajii134@gmail.com>
2025-08-12 11:20:50 +0100

    Add: User schema

---
+++ models/users.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true,
+        minLength: [3, "Name must be at least 3 characters long"],
+        trim: true,
+    },
+
+    username: {
+        type: String,
+        minLength: [3, "Username must be at least 3 characters long"],
+        unique: true,
+        trim: true,
+    },
+
+    password: {
+        type: String,
+        required: true,
+        minLength: [6, "Password must be at least 6 characters long"],
+        maxLength: [20, "Password must be at most 20 characters long"],
+        trim: true,
+    },
+
+    position: {
+        type: String,
+        required: true,
+        default: "President",
+        trim: true,
+    },
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
