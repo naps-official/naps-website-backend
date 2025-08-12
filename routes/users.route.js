a6dd4fcebae0456251edc0dfba935b34b93ccc29
PaulAjii <paulajii134@gmail.com>
2025-08-12 16:07:32 +0100

    feat: add deleteUser route to user routes

--- routes/users.route.js
+++ routes/users.route.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 
-import { createUser, getUsers, getUserById } from "../controllers/user.controller.js";
+import { createUser, getUsers, getUserById, deleteUser } from "../controllers/user.controller.js";
 import validateUser from "../middlewares/auth.middleware.js";
 
 const router = Router();
@@ -8,5 +8,6 @@ const router = Router();
 router.get("/", getUsers);
 router.post("/", validateUser("President"), createUser);
 router.get("/:id", getUserById);
+router.delete("/:id", validateUser("President"), deleteUser);
 
 export default router;
