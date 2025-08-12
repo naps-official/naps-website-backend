04a0f398ae637a70e3e394ae74cd151c7c03f476
PaulAjii <paulajii134@gmail.com>
2025-08-12 13:31:11 +0100

    feat: hashed the password on pre save

--- models/users.model.js
+++ models/users.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema({
     fullName: {
@@ -29,6 +30,11 @@ const userSchema = new mongoose.Schema({
         default: "President",
         trim: true,
     },
+});
+
+userSchema.pre("save", async function () {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
 });
 
 const User = mongoose.model("User", userSchema);
