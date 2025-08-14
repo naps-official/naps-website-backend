874bc110ba329a09ef9123540a62aec5f9daf8a9
PaulAjii <paulajii134@gmail.com>
2025-08-14 03:13:06 +0100

    feat: created the getAllNews controller

--- controllers/news.controller.js
+++ controllers/news.controller.js
@@ -1,5 +1,24 @@
 import News from "../models/news.models.js";
 
+export const getAllNews = async (_, res, next) => {
+    try {
+        const news = await News.find();
+
+        // await news.populate({
+        //     path: "author",
+        //     select: "fullName username position -_id"
+        // });
+        
+        res.status(200).json({
+            status: "success",
+            message: "ALl news fetched successfully",
+            data: news,
+        });
+    } catch (error) {
+        next(error);
+    }
+}
+
 export const createNews = async (req, res, next) => {
     try {
         const { title, content, image } = req.body;
