93a6d386258b57f5e933fa2d16b9ef251eddeb17
PaulAjii <paulajii134@gmail.com>
2025-08-14 02:59:55 +0100

    fix: used the news router in app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import connectDB from "./db/connectdb.js";
 import authRouter from "./routes/auth.route.js";
 import newsRouter from "./routes/news.route.js"
 import usersRouter from "./routes/users.route.js"
+import newsRouter from "./routes/news.route.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
 
 const app = express();
@@ -13,8 +14,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/news", newsRouter)
-app.use("/api/v1/users", usersRouter)
+app.use("/api/v1/news", newsRouter);
+app.use("/api/v1/users", usersRouter);
 
 app.use(errorMiddleware);
 
@@ -30,7 +31,7 @@ const startServer = async () => {
             console.log(`Server running successfully on http://localhost:${ PORT }...`);
         });
     } catch (error) {
-        console.error(error.message)
+        console.error(error.message);
     };
 };
 
