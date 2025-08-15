3b396479f3db0901b71b9058ae9bd24345850047
PaulAjii <paulajii134@gmail.com>
2025-08-15 14:54:47 +0100

    fix: added the colon to accept id in update user route

--- routes/users.route.js
+++ routes/users.route.js
@@ -14,7 +14,7 @@ const router = Router();
 router.get("/", getUsers);
 router.post("/", validateUser("President"), createUser);
 router.get("/:id", getUserById);
-router.patch("/id", validateUser("President"), updateUser);
+router.patch("/:id", validateUser("President"), updateUser);
 router.delete("/:id", validateUser("President"), deleteUser);
 
 export default router;
