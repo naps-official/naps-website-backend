42b91caaaa69ef0109547c0c02553bec223f30ca
PaulAjii <paulajii134@gmail.com>
2025-08-15 14:48:12 +0100

    feat: created the update user route

--- routes/users.route.js
+++ routes/users.route.js
@@ -1,6 +1,12 @@
 import { Router } from "express";
 
-import { createUser, getUsers, getUserById, deleteUser } from "../controllers/user.controller.js";
+import {
+  createUser,
+  getUsers,
+  getUserById,
+  deleteUser,
+  updateUser,
+} from "../controllers/user.controller.js";
 import validateUser from "../middlewares/auth.middleware.js";
 
 const router = Router();
@@ -8,6 +14,7 @@ const router = Router();
 router.get("/", getUsers);
 router.post("/", validateUser("President"), createUser);
 router.get("/:id", getUserById);
+router.patch("/id", validateUser("President"), updateUser);
 router.delete("/:id", validateUser("President"), deleteUser);
 
 export default router;
