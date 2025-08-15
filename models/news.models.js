667e972396ecc6fb9c3fc0c19c4a2439effd727e
PaulAjii <paulajii134@gmail.com>
2025-08-15 13:08:37 +0100

    refactor: improve formatting and structure of author field in news model

--- models/news.models.js
+++ models/news.models.js
@@ -1,47 +1,51 @@
 import mongoose from "mongoose";
 
-const newsSchema = new mongoose.Schema({
+const newsSchema = new mongoose.Schema(
+  {
     title: {
-        type: String,
-        required: true,
-        trim: true,
-        minLength: [10, "Title must be at least 10 characters long"]
+      type: String,
+      required: true,
+      trim: true,
+      minLength: [10, "Title must be at least 10 characters long"],
     },
 
     content: {
-        type: String,
-        required: true,
-        trim: true,
-        minLength: [50, "Content must be at least 50 characters long"]
+      type: String,
+      required: true,
+      trim: true,
+      minLength: [50, "Content must be at least 50 characters long"],
     },
 
     image: {
-        type: String,
+      type: String,
     },
 
     publishedAt: {
-        type: Date,
-        default: Date.now,
+      type: Date,
+      default: Date.now,
     },
 
     author: {
+      user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
-        required: true,
+      },
+      fullName: String,
+      username: String,
     },
 
     status: {
-        type: String,
-        enum: ["draft", "published", "archived"],
-        default: "draft",
+      type: String,
+      enum: ["draft", "published", "archived"],
+      default: "draft",
     },
 
     viewCount: {
-        type: Number,
-        default: 0,
-    }
-},
-    { timestamps: true}
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
 );
 
 const News = mongoose.model("New", newsSchema);
