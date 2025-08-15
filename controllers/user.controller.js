d401d8df66f8082aa0512051970262611eb3b1be
PaulAjii <paulajii134@gmail.com>
2025-08-15 12:33:38 +0100

    refactor: clean up formatting and remove unnecessary code in user controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,111 +1,102 @@
 import User from "../models/users.model.js";
 
 export const createUser = async (req, res, next) => {
-    try {
-        const { fullName, position } = req.body;
+  try {
+    const { fullName, position } = req.body;
 
-        if (!fullName || !position) {
-            const error = new Error("All fields are required");
-            error.statusCode = 400;
-            throw error;
-        };
+    if (!fullName || !position) {
+      const error = new Error("All fields are required");
+      error.statusCode = 400;
+      throw error;
+    }
 
-        const username = fullName;
+    const username = fullName;
 
-        const existingUser = await User.findOne({ username });
+    const existingUser = await User.findOne({ username });
 
-        if (existingUser) {
-            const error = new Error("User already exists");
-            error.statusCode = 400;
-            throw error;
-        }
+    if (existingUser) {
+      const error = new Error("User already exists");
+      error.statusCode = 400;
+      throw error;
+    }
 
-        const newUser = new User({
-            fullName,
-            position,
-            username,
-        });
+    const newUser = new User({
+      fullName,
+      position,
+      username,
+    });
 
-        const user = await newUser.save();
-        const userObj = user.toObject();
-        delete userObj.password;
+    const user = await newUser.save();
+    const userObj = user.toObject();
+    delete userObj.password;
 
-        return res.status(201).json({
-            status: "success",
-            message: "User created successfully",
-            data: userObj,
-        });
-    } catch (error) {
-        next(error);
-    };
+    return res.status(201).json({
+      status: "success",
+      message: "User created successfully",
+      data: userObj,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const getUsers = async (_, res, next) => {
-    try {
-        const users = await User.find().select("-password");
+  try {
+    const users = await User.find().select("-password");
 
-        res.status(200).json({
-            status: "success",
-            message: "Users fetched successfully",
-            data: users,
-            total_count: users.length,
-        });
-    } catch (error) {
-        next(error);
-    };
+    res.status(200).json({
+      status: "success",
+      message: "Users fetched successfully",
+      data: users,
+      total_count: users.length,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const getUserById = async (req, res, next) => {
-    try {
-        const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-        if (!id) {
-            const error = new Error("User ID is required");
-            error.statusCode = 400;
-            throw error;
-        }
+    if (!id) {
+      const error = new Error("User ID is required");
+      error.statusCode = 400;
+      throw error;
+    }
 
-        const user = await User.findById(id).select("-password");
+    const user = await User.findById(id).select("-password");
 
-        if (!user) {
-            const error = new Error("User not found");
-            error.statusCode = 404;
-            throw error;
-        }
+    if (!user) {
+      const error = new Error("User not found");
+      error.statusCode = 404;
+      throw error;
+    }
 
-        res.status(200).json({
-            status: "success",
-            message: "User fetched successfully",
-            data: user,
-        });
-    } catch (error) {
-        next(error);
-    };
-}
+    res.status(200).json({
+      status: "success",
+      message: "User fetched successfully",
+      data: user,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const deleteUser = async (req, res, next) => {
-    try {
-        const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-        if (!id) {
-            const error = new Error("User ID is required");
-            error.statusCode = 400;
-            throw error;
-        }
+    const user = await User.findByIdAndDelete(id);
 
-        const user = await User.findOneAndDelete({ _id: id});
+    if (!user) {
+      const error = new Error("User not found");
+      error.statusCode = 404;
+      throw error;
+    }
 
-        if (!user) {
-            const error = new Error("User not found");
-            error.statusCode = 404;
-            throw error;
-        }
-
-        res.status(204).json({
-            status: "success",
-            message: "User deleted successfully",
-        });
-    } catch (error) {
-        next(error);
-    };
-}
+    res.status(204).end();
+  } catch (error) {
+    next(error);
+  }
+};
