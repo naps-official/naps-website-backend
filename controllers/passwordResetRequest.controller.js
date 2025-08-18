a89ddb4df429662390adf3a7f6ba213ad2213e6e
PaulAjii <paulajii134@gmail.com>
2025-08-18 17:03:37 +0100

    feat: get one password reset request

--- controllers/passwordResetRequest.controller.js
+++ controllers/passwordResetRequest.controller.js
@@ -4,13 +4,40 @@ export const getAllRequests = async (_, res, next) => {
   try {
     const requests = await PasswordRequest.find().populate({
       path: "user",
-      select: "-password __v -_id",
+      select: "-password -__v -_id",
     });
 
     res.status(200).json({
       status: "success",
       message: "All password reset requests retrieved successfully",
       data: requests,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getRequest = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const resetRequest = await PasswordRequest.findById(id).populate({
+      path: "user",
+      select: "-password -__v -_id",
+    });
+
+    if (!resetRequest) {
+      const error = new Error(
+        `Password reset request with id ${resetRequest._id} not found`
+      );
+      error.statusCode = 404;
+      throw error;
+    }
+
+    res.status(200).json({
+      status: "success",
+      message: `Password reset request with id ${resetRequest._id} fetched succesfully`,
+      data: resetRequest,
     });
   } catch (error) {
     next(error);
--- routes/passwordResetRequest.route.js
+++ routes/passwordResetRequest.route.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
 
-import { getAllRequests } from "../controllers/passwordResetRequest.controller.js";
+import {
+  getAllRequests,
+  getRequest,
+} from "../controllers/passwordResetRequest.controller.js";
 import verifyUser from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.get("/", verifyUser(), getAllRequests);
+router.get("/:id", verifyUser(), getRequest);
 
 export default router;
