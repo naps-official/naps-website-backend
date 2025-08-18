d92a68e9ba30b070faa10fb2a1acc09d189d73a6
PaulAjii <paulajii134@gmail.com>
2025-08-18 14:40:08 +0100

    fix: moved the change password route to auth route file

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import { signIn } from "../controllers/auth.controller.js";
+import validateUser from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.post("/login", signIn);
+router.post("/change-password", validateUser(), changePassword);
 
 export default router;
