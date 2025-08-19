946970a69a8927c410faea88bcad67de090cfbc8
PaulAjii <paulajii134@gmail.com>
2025-08-19 11:58:38 +0100

    chore: added the cloudinary sdk

--- config/env.js
+++ config/env.js
@@ -1,4 +1,16 @@
 import { config } from "dotenv";
-config({path: `.env.${process.env.NODE_ENV || "development"}.local`}, {debug: true})
+config(
+  { path: `.env.${process.env.NODE_ENV || "development"}.local` },
+  { debug: true }
+);
 
-export const { PORT, NODE_ENV, MONGO_URI, JWT_SECRET, JWT_EXPIRATION } = process.env
+export const {
+  PORT,
+  NODE_ENV,
+  MONGO_URI,
+  JWT_SECRET,
+  JWT_EXPIRATION,
+  CLOUDINARY_CLOUD_NAME,
+  CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET,
+} = process.env;
