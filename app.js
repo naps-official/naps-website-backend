7b739ef86fc324991ab8a05b70c2c2a7a03eb212
PaulAjii <paulajii134@gmail.com>
2025-08-19 12:07:12 +0100

    feat: setup cloudinary configs

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 import express from "express";
+import { v2 as cloudinary } from "cloudinary";
 
-import { PORT, MONGO_URI } from "./config/env.js";
+import {
+  PORT,
+  MONGO_URI,
+  CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET,
+  CLOUDINARY_CLOUD_NAME,
+} from "./config/env.js";
 import connectDB from "./db/connectdb.js";
 import authRouter from "./routes/auth.route.js";
 import newsRouter from "./routes/news.route.js";
 import usersRouter from "./routes/users.route.js";
 import passwordRequestRouter from "./routes/passwordResetRequest.route.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
+
+cloudinary.config({
+  cloud_name: CLOUDINARY_CLOUD_NAME,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET,
+  secure: true,
+});
 
 const app = express();
 
